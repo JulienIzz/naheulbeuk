086a4d6de55155be77aded76c00c3a709640a186
JulienIzz <julien.izzillo@gmail.com>
2026-03-11 13:47:49 +0100

    feat: error page design

--- src/shared/components/QueryErrorBoundary.tsx
+++ src/shared/components/QueryErrorBoundary.tsx
@@ -1,12 +1,14 @@
 import styled from "@emotion/native";
 import { useTheme } from "@emotion/react";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useLingui } from "@lingui/react/macro";
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
 import type { ReactNode } from "react";
 import { Suspense } from "react";
 import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
-import { Pressable } from "react-native";
 import { ActivityIndicator } from "react-native";
 
+import { Button } from "#design-system/components/Button";
 import { Typography } from "#design-system/typography/Typography";
 
 type Props = {
@@ -20,9 +22,13 @@ export const QueryErrorBoundary = ({
   loadingFallback = <DefaultLoadingFallback />,
   FallbackComponent = DefaultErrorFallback,
 }: Props) => (
-  <ErrorBoundary FallbackComponent={FallbackComponent}>
-    <Suspense fallback={loadingFallback}>{children}</Suspense>
-  </ErrorBoundary>
+  <QueryErrorResetBoundary>
+    {({ reset }) => (
+      <ErrorBoundary onReset={reset} FallbackComponent={FallbackComponent}>
+        <Suspense fallback={loadingFallback}>{children}</Suspense>
+      </ErrorBoundary>
+    )}
+  </QueryErrorResetBoundary>
 );
 
 const DefaultErrorFallback = ({ resetErrorBoundary }: FallbackProps) => {
@@ -31,15 +37,26 @@ const DefaultErrorFallback = ({ resetErrorBoundary }: FallbackProps) => {
 
   return (
     <Container>
-      <Typography variant="P1" text={t`Une erreur est survenue`} />
-      <Pressable onPress={resetErrorBoundary}>
-        <Typography
-          variant="P1"
-          text={t`Réessayer`}
-          bold
-          color={theme.colors.secondary}
+      <ContentContainer>
+        <MaterialIcons
+          name="error-outline"
+          size={theme.icons.l}
+          color={theme.colors.wood}
         />
-      </Pressable>
+        <Typography
+          variant="H2"
+          text={t`Une erreur est survenue`}
+          color={theme.colors.wood}
+          center
+        />
+        <Typography
+          variant="P2"
+          text={t`Vérifiez votre connexion et réessayez`}
+          color={theme.colors.wood}
+          center
+        />
+      </ContentContainer>
+      <Button label={t`Réessayer`} onPress={resetErrorBoundary} />
     </Container>
   );
 };
@@ -54,5 +71,11 @@ const Container = styled.View(({ theme }) => ({
   flex: 1,
   justifyContent: "center",
   alignItems: "center",
-  gap: theme.spacing.xl,
+  gap: theme.spacing.xxxl,
+  padding: theme.spacing.xxxl,
+}));
+
+const ContentContainer = styled.View(({ theme }) => ({
+  alignItems: "center",
+  gap: theme.spacing.l,
 }));
